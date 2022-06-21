59fd05150f09f5361b3e68430225b11bb60df2b2
davincecode <hyperscript.ca@gmail.com>
2022-06-20 22:46:07 -0600

    Update Chefs container

--- src/constants/data.js
+++ src/constants/data.js
@@ -4,27 +4,27 @@ import images from './images';
   const wines = [
     {
       title: 'Chapel Hill Shiraz',
-      price: '$56',
+      price: '$156',
       tags: 'AU | Bottle',
     },
     {
       title: 'Catena Malbee',
-      price: '$59',
+      price: '$159',
       tags: 'AU | Bottle',
     },
     {
       title: 'La Vieillw Rose',
-      price: '$44',
+      price: '$144',
       tags: 'FR | 750 ml',
     },
     {
       title: 'Rhino Pale Ale',
-      price: '$31',
+      price: '$131',
       tags: 'CA | 750 ml',
     },
     {
       title: 'Irish Guinness',
-      price: '$26',
+      price: '$126',
       tags: 'IE | 750 ml',
     },
   ];
@@ -32,27 +32,27 @@ import images from './images';
   const cocktails = [
     {
       title: 'Aperol Sprtiz',
-      price: '$20',
+      price: '$120',
       tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
     },
     {
       title: "Dark 'N' Stormy",
-      price: '$16',
+      price: '$116',
       tags: 'Dark rum | Ginger beer | Slice of lime',
     },
     {
       title: 'Daiquiri',
-      price: '$10',
+      price: '$110',
       tags: 'Rum | Citrus juice | Sugar',
     },
     {
       title: 'Old Fashioned',
-      price: '$31',
+      price: '$131',
       tags: 'Bourbon | Brown sugar | Angostura Bitters',
     },
     {
       title: 'Negroni',
-      price: '$26',
+      price: '$126',
       tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
     },
   ];
