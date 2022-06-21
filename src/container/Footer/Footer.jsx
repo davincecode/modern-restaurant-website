b2fe0db2e9a5fe8a3ac9209c00681c3850b1132d
davincecode <hyperscript.ca@gmail.com>
2022-06-20 23:45:34 -0600

    Update footer components

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -14,7 +14,8 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__opensans">Center St. Bungalow, Whatcroft Hall</p>
+        <p className="p__opensans">Lane, Rudheath, CW9 75G</p>
         <p className="p__opensans">+1 212-344-1230</p>
         <p className="p__opensans">+1 212-555-1230</p>
       </div>
@@ -33,9 +34,9 @@ const Footer = () => (
       <div className="app__footer-links_work">
         <h1 className="app__footer-headtext">Working Hours</h1>
         <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">10:00 am - 02:00 am</p>
         <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00 am - 11:00 pm</p>
+        <p className="p__opensans">10:00 am - 03:00 am</p>
       </div>
     </div>
 
